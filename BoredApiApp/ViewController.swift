b3c2bd42b6479e827b3f1eed5df45d000eb36527
Nguyen Loc <loc9493@gmail.com>
2022-03-12 11:24:29 +0700

    Initial Commit

---
+++ BoredApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoredApiApp
+//
+//  Created by Nguyen Loc on 3/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
